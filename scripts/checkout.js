bfbbd9aca7b9c53b78cf27662b43a09c083eb2a2
omarahorrar <omarahorar@gmail.com>
2025-04-13 14:57:53 +0100

    save cart to LocalStorage

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
-import {formatCurrency } from './utils/money.js';
+import {formatCurrency} from './utils/money.js';
 
 let cartSummaryHTML = ""
 
@@ -102,7 +102,7 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
         const productId = link.dataset.productId;
         removeFromCart(productId)
 
-        const container = document.querySelector(`js-cart-item-container-${productId}`)
+        const container = document.querySelector(`.js-cart-item-container-${productId}`)
         container.remove()
     })
 })
