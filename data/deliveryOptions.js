feacd0b72fde29d792a41c0c91fd81ba59ccf318
omarahorrar <omarahorar@gmail.com>
2025-04-14 00:47:54 +0100

    save data and generate HTML for delivery options

---
+++ data/deliveryOptions.js
@@ -0,0 +1,16 @@
+export const deliveryOptions = [{
+    id: '1',
+    deliveryDays: 7,
+    priceCents: 0,
+    deliveryOptionId: '1'
+}, {
+    id: '2',
+    deliveryDays: 3,
+    priceCents: 499,
+    deliveryOptionId: '1'
+}, {
+    id: '3',
+    deliveryDays: 1,
+    priceCents: 999,
+    deliveryOptionId: '1'
+}];
